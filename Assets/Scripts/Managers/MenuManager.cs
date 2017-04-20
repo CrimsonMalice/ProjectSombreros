6f82e031e29f2d8288e7036bcc46fd719fe9c357
Glänn <hallda@utb.fuv.se>
2017-04-20 18:32:42 +0200

    * Added movie for test purpose

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour {
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private bool isPlaying;
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        Renderer renderer = GetComponent<Renderer>();
+        MovieTexture movie = (MovieTexture)renderer.material.mainTexture;
+        isPlaying = true;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        if (Input.GetButtonDown("Enter"))
+        {
+            LoadLevel();
+        }
 	}
+
+    void OnAwake()
+    {
+
+    }
 
     public void LoadLevel()
     {
