1d0f8c8dcc841c9877677588ab60d3a16d410a50
CrimsonMalice <alexandermalusbohlin@gmail.com>
2017-05-09 19:00:23 +0100

    Added so that you don't have to click & drag AI-points in the inspector anymore.

--- Assets/Scripts/Enemies/PatrolPoint.cs
+++ Assets/Scripts/Enemies/PatrolPoint.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class PatrolPoint : MonoBehaviour {
 
-    [SerializeField] private GameObject[] adjacentPoints; //Adjacent points to this PatrolPoint. Set these in the Unity inspector.
+    [SerializeField] private List<GameObject> adjacentPoints; //Adjacent points to this PatrolPoint. Set these in the Unity inspector.
 
-    public GameObject[] AdjacentPoints { get { return this.adjacentPoints; } }
+    public List<GameObject> AdjacentPoints { get { return this.adjacentPoints; } }
+    private List<float> adjacentPointsDistanceX = new List<float>();
+    private List<float> adjacentPointsDistanceY = new List<float>();
+    private List<float> adjacentNegativePointsDistanceX = new List<float>();
+    private List<float> adjacentNegativePointsDistanceY = new List<float>();
+    private float pointsAmount = 2;
+
+    bool minusX = false;
+    bool minusY = false;
 
     // Use this for initialization
     void Start ()
     {
-		
+        List<GameObject> patrolPoints = new List<GameObject>();
+
+        //Find all of the PatrolPoints with the same X and Y values.
+        foreach (GameObject pp in GameObject.FindGameObjectsWithTag("PatrolPoint"))
+        {
+            if (pp.transform.position.x == gameObject.transform.position.x)
+            {
+                patrolPoints.Add(pp);
+            }
+            else if (pp.transform.position.y == gameObject.transform.position.y)
+            {
+                patrolPoints.Add(pp);
+            }
+        }
+
+        for (int i = 0; i < patrolPoints.Count; i++)
+        {
+            if (gameObject.name == patrolPoints[i].gameObject.name)
+            {
+                patrolPoints.RemoveAt(i);
+            }
+        }
+
+        adjacentPoints = patrolPoints; //REMOVE AFTER CODE ABOVE IS FINISHED
 	}
 	
 	// Update is called once per frame
