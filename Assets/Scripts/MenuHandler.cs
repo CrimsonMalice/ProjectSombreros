2d7707ff7a2e736c46207bc0be12a1bf539d6138
glänn <david@waowstudios.com>
2017-05-14 23:57:57 +0200

    UI fix

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    [SerializeField] int level;
+	
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene(level);
+
+  
+    }
+
+    public void QuitGame()
+    {
+        //Application.Quit;
+    }
+
+    void OnGUI()
+    {
+        
+    }
+}
