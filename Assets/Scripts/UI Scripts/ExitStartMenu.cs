4e945f5e1708db48388fa3d45cd90d6089307ae5
CrimsonMalice <alexandermalusbohlin@gmail.com>
2017-05-16 22:56:45 +0100

    no message

---
+++ Assets/Scripts/UI Scripts/ExitStartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitStartMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject mainMenuPic;
+    [SerializeField] private GameObject itemFrame;
+    [SerializeField] private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainMenuPic.SetActive(false);
+        itemFrame.SetActive(false);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mainMenuPic.SetActive(true);
+            itemFrame.SetActive(true);
+
+            gameObject.SetActive(false);
+            player.SetActive(false);
+        }
+	}
+}
