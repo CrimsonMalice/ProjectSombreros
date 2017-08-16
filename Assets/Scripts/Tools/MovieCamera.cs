6c026f95d5e8b7ed5e952cff7cc6face4e9a2eed
CrimsonMalice <malusnosferatu@gmail.com>
2017-08-16 17:12:42 +0200

    HERPADERP

---
+++ Assets/Scripts/Tools/MovieCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieCamera : MonoBehaviour {
+
+    PlayerController pc;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pc = GameObject.FindGameObjectWithTag("PlayerOne").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.position = new Vector3(pc.transform.position.x, pc.transform.position.y, -10f);
+	}
+}
